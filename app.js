a738727d574c54ba39325224a45ce9ca8533bb47
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-03-05 16:33:49 +0700

    adding EJS template engine and views files

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+const path = require("path");
+
 const express = require("express");
 const authRoutes = require("./controllers/auth-controller");
 
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(authRoutes);
 
