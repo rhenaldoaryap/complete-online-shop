b319bb62f2e341dae1624887c259d16a06faee47
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-03-06 14:31:19 +0700

    adding csrf protection

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const path = require("path");
 
 const express = require("express");
+const csrf = require("csurf");
+
 const db = require("./data/database");
+const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
@@ -10,9 +13,10 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static("public"));
-
 app.use(express.urlencoded({ extended: false }));
 
+app.use(csrf());
+app.use(addCsrfTokenMiddleware);
 app.use(authRoutes);
 
 db.connectToDatabase()
