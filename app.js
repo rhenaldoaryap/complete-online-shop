fa14c3ff455b61a6e329d2ea3ba49780fe239dcb
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-03-06 10:34:00 +0700

    adding user signup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 const express = require("express");
+const db = require("./data/database");
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
@@ -10,6 +11,15 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static("public"));
 
+app.use(express.urlencoded({ extended: false }));
+
 app.use(authRoutes);
 
-app.listen(3000);
+db.connectToDatabase()
+  .then(function () {
+    app.listen(3000);
+  })
+  .catch(function (error) {
+    console.log("Failed connect to database");
+    console.log(error);
+  });
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router.get("/signup", authController.getSignup);
 
+router.post("/signup", authController.signup);
+
 router.get("/login", authController.getLogin);
 
 module.exports = router;
