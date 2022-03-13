c7eb22323af1a66fa9da29e91691857c1305c175
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-03-13 14:59:43 +0700

    outputting products for customers

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
+const productControllers = require("../controllers/products-controller");
+
 const router = express.Router();
 
-router.get("/products", function (req, res) {
-  res.render("customer/products/all-products");
-});
+router.get("/products", productControllers.getAllProducts);
 
 module.exports = router;
