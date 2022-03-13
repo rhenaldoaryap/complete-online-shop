5d47977e3109c8af413b8434c073f3edf92d9e37
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-03-13 15:30:02 +0700

    outputting product details

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -6,4 +6,6 @@ const router = express.Router();
 
 router.get("/products", productControllers.getAllProducts);
 
+router.get("/products/:id", productControllers.getProductDetails);
+
 module.exports = router;
