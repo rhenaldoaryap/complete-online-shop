a99ea248612e0ba4f82f2ca52731f12e704b8f5c
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-03-14 16:28:11 +0700

    adding cart page

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -3,6 +3,8 @@ const cartController = require("../controllers/cart-controller");
 
 const router = express.Router();
 
+router.get("/", cartController.getCart); // "/cart/"
+
 router.post("/items", cartController.addCartItem);
 
 module.exports = router;
